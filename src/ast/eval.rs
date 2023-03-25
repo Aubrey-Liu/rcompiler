92aad1eff9638c9dde4bfbf056957dda43b25806
Aubrey-Liu <aubreyliu233@gmail.com>
2023-03-25 09:53:14 +0800

    More clean up

---
+++ src/ast/eval.rs
@@ -0,0 +1,27 @@
+use super::{BinaryOp, UnaryOp};
+
+pub fn eval_binary(op: BinaryOp, lhs: i32, rhs: i32) -> i32 {
+    match op {
+        BinaryOp::Add => lhs + rhs,
+        BinaryOp::Sub => lhs - rhs,
+        BinaryOp::Mul => lhs * rhs,
+        BinaryOp::Div => lhs / rhs,
+        BinaryOp::Mod => lhs % rhs,
+        BinaryOp::And => (lhs != 0 && rhs != 0) as i32,
+        BinaryOp::Or => (lhs != 0 || rhs != 0) as i32,
+        BinaryOp::Eq => (lhs == rhs) as i32,
+        BinaryOp::Neq => (lhs != rhs) as i32,
+        BinaryOp::Lt => (lhs < rhs) as i32,
+        BinaryOp::Le => (lhs <= rhs) as i32,
+        BinaryOp::Gt => (lhs > rhs) as i32,
+        BinaryOp::Ge => (lhs >= rhs) as i32,
+    }
+}
+
+pub fn eval_unary(op: UnaryOp, rhs: i32) -> i32 {
+    match op {
+        UnaryOp::Nop => rhs,
+        UnaryOp::Neg => -rhs,
+        UnaryOp::Not => (rhs == 0) as i32,
+    }
+}
--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -1,9 +1,9 @@
 pub(crate) use block::*;
+pub(crate) use eval::{eval_binary, eval_unary};
 pub(crate) use exp::*;
 pub(crate) use func::*;
-pub(crate) use utils::{eval_binary, eval_unary};
 
 pub(crate) mod block;
+pub(crate) mod eval;
 pub(crate) mod exp;
 pub(crate) mod func;
-pub(crate) mod utils;
